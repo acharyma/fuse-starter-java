b2d6229a58c342b44c7739e6f722e12c9c46a1bf
macharya <acharyma@bc.edu>
2021-11-22 13:46:15 -0500

    Issue 3 - WIP: trying to create a repository for Historical Prices

---
+++ src/main/java/org/galatea/starter/domain/rpsy/HistoricalPricesRpsy.java
@@ -0,0 +1,19 @@
+package org.galatea.starter.domain.rpsy;
+
+import java.util.List;
+import java.util.Optional;
+import org.galatea.starter.domain.IexHistoricalPrices;
+import org.springframework.data.repository.CrudRepository;
+
+public interface HistoricalPricesRpsy extends CrudRepository<IexHistoricalPrices, Long> {
+  /**
+   * Retrieves all entities with the given symbol.
+   */
+  List<IexHistoricalPrices> findBySymbol(String symbol);
+
+  /**
+   * Saves historical prices I think.
+   * @param entity the historical price to save
+   */
+  <S extends IexHistoricalPrices> S save(S entity);
+}
